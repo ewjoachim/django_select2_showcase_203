78fbf4fc12a8ed071f3ba7269452ea16d74869d8
Joachim Jablon <joachim@botify.com>
2023-02-09 14:06:27 +0100

    Making it even clearer

--- bugshowcase/select2.py
+++ bugshowcase/select2.py
@@ -1,6 +1,5 @@
-from django.conf import settings
 from django_select2 import forms
 
 
-def func():
-    return forms.Select2Widget
+def get_widget():
+    return forms.Select2Widget()
---
+++ bugshowcase/tests/test_widget.py
@@ -0,0 +1,17 @@
+from django.test import TestCase, override_settings
+from django.conf import settings
+
+from unittest.mock import patch
+
+
+class TestShowcase(TestCase):
+    @override_settings(A=1)
+    @patch("bugshowcase.select2.get_widget", autospec=True)
+    def test_1(self, s2):
+        self.assertEqual(1 + 1, 2)
+
+    def test_2(self):
+        from bugshowcase.select2 import get_widget
+
+        widget = get_widget()
+        self.assertNotEqual(widget.render(name="foo", value="bar"), "")
